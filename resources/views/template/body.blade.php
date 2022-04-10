537d8568196a88d3cdb5bced512303865fda3765
Martanto <martanto@live.com>
2022-04-10 16:35:24 +0700

    wip

--- resources/views/template/body.blade.php
+++ resources/views/template/body.blade.php
@@ -111,6 +111,7 @@
                                         </li>
                                         <hr>
                                         <li><a href="">Permohonan Data</a></li>
+                                        <li><a href="">Permohonan API Service MAGMA</a></li>
                                         <li><a href="">Praktik Kerja Lapangan</a></li>
                                         <li><a href="">Bimbingan Tugas Akhir</a></li>
                                         <hr>
@@ -279,40 +280,241 @@
     </div>
     <!--CITY MAIN BANNER END-->
 
-    <!--CITY BANNER SERVICES START-->
     <div class="city_banner_services">
         <div class="container-fluid">
             <div class="city_service_list">
                 <ul>
                     <li>
                         <div class="city_service_text">
-                            <span><i class="fa icon-news"></i></span>
-                            <h5><a href="#">News & Public Notices</a></h5>
+                            <span><i class="fa fa-map"></i></span>
+                            <h5><a href="#">MAGMA Indonesia</a></h5>
                         </div>
                     </li>
                     <li>
                         <div class="city_service_text">
-                            <span><i class="fa icon-law"></i></span>
-                            <h5><a href="#">Goverment Tender </a></h5>
+                            <span><i class="fa icon-news"></i></span>
+                            <h5><a href="#">Informasi Bencana Geologi</a></h5>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="city_service_text">
+                            <span><i class="fa fa-download"></i></span>
+                            <h5><a href="#">Download Peta KRB</a></h5>
                         </div>
                     </li>
                     <li>
                         <div class="city_service_text">
                             <span><i class="fa icon-cursor"></i></span>
-                            <h5><a href="#">Pay Online Tickets</a></h5>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="city_service_text">
-                            <span><i class="fa icon-trash"></i></span>
-                            <h5><a href="#">Garbage and Recycling</a></h5>
+                            <h5><a href="#">Lapor Bencana</a></h5>
                         </div>
                     </li>
                 </ul>
             </div>
         </div>
     </div>
-    <!--CITY BANNER SERVICES END-->
+
+    <div class="city_jobs_wrap" style="background-image: url('https://magma.vsi.esdm.go.id/img/crs/VEN_LEW20220214142737.png')">
+        <div class="city_jobs_fig">
+            <div class="city_job_text">
+                <h3>Informasi Letusan</h3>
+                <h2>Gunung Api Ibu</h2>
+                <p>This is Photoshop's version of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean
+                    sollicitudin, lorem quis </p>
+                <a class="theam_btn_yellow" href="#" tabindex="0">Get In Touch</a>
+            </div>
+        </div>
+        <div class="city_jobs_list">
+            <ul>
+                <li>
+                    <div class="city_jobs_item overlay">
+                        <span><i class="fa icon-volcano-warning" style="font-family: 'magma' !important;"></i></span>
+                        <div class="ciy_jobs_caption">
+                            <h2>Laporan Gunung Api</h2>
+                            <p>This is Photoshop's ersion of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
+                                Aenean sollicitudin</p>
+                            <a href="#">Find Out More</a>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="city_jobs_item pull-right overlay">
+                        <div class="ciy_jobs_caption ciy_jobs_caption_dark">
+                            <h2>Tanggapan Kejadian Gerakan Tanah</h2>
+                            <p>This is Photoshop's ersion of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
+                                Aenean sollicitudin</p>
+                            <a href="#">Find Out More</a>
+                        </div>
+                        <span><i class="fa icon-landslide" style="font-family: 'magma' !important;"></i></span>
+                    </div>
+                </li>
+                <li>
+                    <div class="city_jobs_item overlay">
+                        <span><i class="fa icon-earthquake" style="font-family: 'magma' !important;"></i></span>
+                        <div class="ciy_jobs_caption">
+                            <h2>Kajian Kejadian Gempa Bumi dan Tsunami</h2>
+                            <p>This is Photoshop's ersion of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
+                                Aenean sollicitudin</p>
+                            <a href="#">Find Out More</a>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="city_news_wrap">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <!--SECTION HEADING START-->
+                    <div class="section_heading margin-bottom">
+                        <span>Welcome to Local City</span>
+                        <h2>News Releases</h2>
+                    </div>
+                    <!--SECTION HEADING START-->
+                    <div class="row">
+                        <div class="col-md-6 col-sm-6">
+                            <div class="city_news_fig">
+                                <figure class="box">
+                                    <div class="box-layer layer-1"></div>
+                                    <div class="box-layer layer-2"></div>
+                                    <div class="box-layer layer-3"></div>
+                                    <img src="extra-images/news-fig.jpg" alt="">
+                                </figure>
+                                <div class="city_news_text">
+                                    <h2>A Fundraiser for the City Club</h2>
+                                    <ul class="city_news_meta">
+                                        <li><a href="#">May 22, 2018</a></li>
+                                        <li><a href="#">Public Notices</a></li>
+                                    </ul>
+                                    <p>Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sollicitudin</p>
+                                    <a class="theam_btn border-color color" href="#" tabindex="0">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-sm-6">
+                            <div class="city_news_row">
+                                <ul>
+                                    <li>
+                                        <div class="city_news_list">
+                                            <figure class="box">
+                                                <div class="box-layer layer-1"></div>
+                                                <div class="box-layer layer-2"></div>
+                                                <div class="box-layer layer-3"></div>
+                                                <img src="extra-images/news-fig1.jpg" alt="">
+                                            </figure>
+                                            <div class="city_news_list_text">
+                                                <h5>Lorem Ipsum Proin gravida nibh </h5>
+                                                <ul class="city_news_meta">
+                                                    <li><a href="#">May 22, 2018</a></li>
+                                                    <li><a href="#">Public Notices</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="city_news_list">
+                                            <figure class="box">
+                                                <div class="box-layer layer-1"></div>
+                                                <div class="box-layer layer-2"></div>
+                                                <div class="box-layer layer-3"></div>
+                                                <img src="extra-images/news-fig2.jpg" alt="">
+                                            </figure>
+                                            <div class="city_news_list_text">
+                                                <h5>Lorem Ipsum Proin gravida nibh </h5>
+                                                <ul class="city_news_meta">
+                                                    <li><a href="#">May 22, 2018</a></li>
+                                                    <li><a href="#">Public Notices</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="city_news_list">
+                                            <figure class="box">
+                                                <div class="box-layer layer-1"></div>
+                                                <div class="box-layer layer-2"></div>
+                                                <div class="box-layer layer-3"></div>
+                                                <img src="extra-images/news-fig3.jpg" alt="">
+                                            </figure>
+                                            <div class="city_news_list_text">
+                                                <h5>Lorem Ipsum Proin gravida nibh </h5>
+                                                <ul class="city_news_meta">
+                                                    <li><a href="#">May 22, 2018</a></li>
+                                                    <li><a href="#">Public Notices</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="city_news_list">
+                                            <figure class="box">
+                                                <div class="box-layer layer-1"></div>
+                                                <div class="box-layer layer-2"></div>
+                                                <div class="box-layer layer-3"></div>
+                                                <img src="extra-images/news-fig4.jpg" alt="">
+                                            </figure>
+                                            <div class="city_news_list_text">
+                                                <h5>Lorem Ipsum Proin gravida nibh </h5>
+                                                <ul class="city_news_meta">
+                                                    <li><a href="#">May 22, 2018</a></li>
+                                                    <li><a href="#">Public Notices</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="city_news_list">
+                                            <figure class="box">
+                                                <div class="box-layer layer-1"></div>
+                                                <div class="box-layer layer-2"></div>
+                                                <div class="box-layer layer-3"></div>
+                                                <img src="extra-images/news-fig5.jpg" alt="">
+                                            </figure>
+                                            <div class="city_news_list_text">
+                                                <h5>Lorem Ipsum Proin gravida nibh </h5>
+                                                <ul class="city_news_meta">
+                                                    <li><a href="#">May 22, 2018</a></li>
+                                                    <li><a href="#">Public Notices</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="city_news_form">
+                        <div class="city_news_feild">
+                            <span>Signup</span>
+                            <h4>Newsletter</h4>
+                            <p>This is Photoshop's version  of Lorem Ipsum. Proin gravida </p>
+                            <div class="city_news_search">
+                                <input type="text" name="text" placeholder="Enter Your Email Adress Here" required="">
+                                <button class="theam_btn border-color color">Subcribe Now</button>
+                            </div>
+                        </div>
+                        <div class="city_news_feild feild2">
+                            <span>Recent</span>
+                            <h4>Documents</h4>
+                            <p>This is Photoshop's version  of Lorem Ipsum. Proin gravida </p>
+                            <div class="city_document_list">
+                                <ul>
+                                    <li><a href="#"><i class="fa icon-document"></i>Council Agenda April 24, 2015 (27 kB)</a></li>
+                                    <li><a href="#"><i class="fa icon-document"></i>Council Agenda April 24, 2015 (27 kB)</a></li>
+                                    <li><a href="#"><i class="fa icon-document"></i>Council Agenda April 24, 2015 (27 kB)</a></li>
+                                    <li><a href="#"><i class="fa icon-document"></i>Council Agenda April 24, 2015 (27 kB)</a></li>
+                                    <li><a href="#"><i class="fa icon-document"></i>Council Agenda April 24, 2015 (27 kB)</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
     @include('template.footer')
 </div>
--- resources/views/template/head.blade.php
+++ resources/views/template/head.blade.php
@@ -25,6 +25,7 @@
     <link href="{{ asset('css/shotcode.css') }}" rel="stylesheet">
     <link href="{{ asset('svg-icon.css') }}" rel="stylesheet">
     <link href="{{ asset('css/color.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/icon-magma.css') }}" rel="stylesheet">
     <link href="{{ asset('css/vsi.css') }}" rel="stylesheet">
     @stack('styles')
     <link href="{{ asset('css/responsive.css') }}" rel="stylesheet">
