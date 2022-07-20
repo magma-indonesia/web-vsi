0e55713eeb2fde4e3fe52d61250094ec18c1ee04
Martanto <martanto@live.com>
2022-07-20 17:19:41 +0700

    wip

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -29,10 +29,6 @@ class Kernel extends HttpKernel
      * @var array<string, array<int, class-string|string>>
      */
     protected $middlewareGroups = [
-        'test' => [
-            \App\Http\Middleware\TestMiddleware::class,
-        ],
-
         'web' => [
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
@@ -52,6 +48,11 @@ class Kernel extends HttpKernel
         'dashboard' => [
             'web',
             'auth',
+        ],
+
+        'test' => [
+            'dashboard',
+            \App\Http\Middleware\TestMiddleware::class,
         ],
     ];
 
